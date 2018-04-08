e85e86e18f3ecaff03008f0316916a99e024cb40
Tristan A.A <t.aumentado@live.ca>
2018-04-08 02:41:25 -0400

    Fixed bug -_- and did some cleanup

--- PolicyBasedTree-2.py
+++ PolicyBasedTree-2.py
@@ -11,6 +11,7 @@ from LSTMFM2 import LSTMForwardModel
 
 import os, sys, pickle, numpy as np, numpy.random as npr, random as r
 
+##############################################
 def sample_gumbel(shape, eps=1e-20):
     U = torch.rand(shape) #.cuda()
     return -avar(torch.log(-torch.log(U + eps) + eps))
@@ -31,14 +32,13 @@ def gumbel_softmax(logits, temperature):
     y_hard.scatter_(1, ind.view(-1, 1), 1)
     y_hard = y_hard.view(*shape)
     return y #+ (y_hard - y).detach() 
+##############################################
+
 
 def greedy_valueF(state):
     state = state.squeeze()
-    #ForwardModel.printState(state)
     vx = torch.sum((state[0:15]-state[34:49]).pow(2))
-    #print('vx',vx)
     vy = torch.sum((state[15:30]-state[49:64]).pow(2))
-    #print('vy',vy)
     value = -( vx + vy ) 
     return value
 
@@ -48,11 +48,8 @@ def greedy_cont_valueF(state):
     _,gx = state[34:49].max(0)
     _,iy = state[15:30].max(0)
     _,gy = state[49:64].max(0)
-    #ForwardModel.printState(state)
     vx = torch.sum((ix - gx)*(ix - gx))
-    #print('vx',vx)
     vy = torch.sum((iy - gy)*(iy - gy))
-    #print('vy',vy)
     value = -( vx + vy ) 
     return value
 
@@ -60,22 +57,13 @@ def greedy_CE(state):
     state = state.squeeze()
     _, gx = state[34:49].max(0)
     _, gy = state[49:64].max(0)
-    
     px = state[0:15]
     py = state[15:30]
-    
     loss = torch.nn.CrossEntropyLoss()
     vx = loss(px.unsqueeze(dim=0), gx)
     vy  = loss(py.unsqueeze(dim=0), gy)
-#     print(vx,vy)
     return - (vx + vy)
     
-def generateTask(px,py,orien,gx,gy):
-    direction = NavigationTask.oriens[orien]
-    gs = np.array([gx, gy])
-    env = NavigationTask(agent_start_pos=[np.array([px,py]), direction],goal_pos=gs)
-    return env
-
 def generateTask(px,py,orien,gx,gy):
     direction = NavigationTask.oriens[orien]
     gs = np.array([gx, gy])
@@ -113,66 +101,83 @@ class SimulationPolicy(nn.Module):
         output = F.softmax(output,dim=1)
         return output
     
-    def trainSad(self, taskEnv, forwardModel, printActions=False, maxDepth=5, treeBreadth=2, eta_lr=0.0005):
+    def trainSad(self, 
+        taskEnv, 
+        forwardModel, 
+        printActions=False, 
+        maxDepth=5, 
+        treeBreadth=2, 
+        eta_lr=0.0005,
+        trainIters=500,
+        alpha=0.5,
+        lambda_h=-0.025,
+        useHolder=False,
+        holderp=-6.0, 
+        useOnlyLeaves=False
+        ):
         optimizer = optim.Adam(self.parameters(), lr = eta_lr )
         for p in forwardModel.parameters(): p.requires_grad = False
-#         p = npr.randint(0,15,2)
-#         orien = npr.randint(0,4,1)
-#         g = npr.randint(0,15,2)
-#        cenv = generateTask(0,0,0,7,7)
-#       cenv = generateTask(p[0],p[1],orien,g[0],g[1])
         s0 = avar(torch.FloatTensor([self.env.getStateRep()]), requires_grad=False)
-        for i in range(0,1000):
-            tree = Tree(s0,forwardModel,self,greedy_valueF, self.env,maxDepth,treeBreadth)
-            loss = tree.getLossFromLeaves()
+        for i in range(0,trainIters):
+            tree = Tree(s0, forwardModel, self,greedy_valueF, self.env, maxDepth, treeBreadth)
+            loss = tree.getLossFromAllNodes(
+                alpha=alpha, lambda_h=lambda_h, useHolder=useHolder, 
+                holderp=holderp, useOnlyLeaves=useOnlyLeaves
+            )
+            # print(i,'loss',loss)
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
             if i % 50 == 0: 
                 print('Loss',i,":",loss.data[0])
                 if printActions:
-                    print( "\n".join([ ",".join([str(qq) for qq in a[0].data.numpy()]) for a in tree.getBestPlan()[1]] ) )
+                    plan = tree.getBestPlan()
+                    # print(plan)
+                    print( "\n".join([ "A" + str(qi) + ": " + 
+                        ",".join([
+                            ",".join(["%.3f" % q for q in qq]) for qq in a[0].data.numpy()
+                        ]) for qi,a in enumerate(plan[1])
+                    ] ) )
         
 # POSSIBLE IDEA
 # Dont just consider the leaves; consider all the nodes as possible leaves (consider all subpaths too)
 
 class Node(object):
     
-    def __init__(self, parent_node, state, action, hidden):
+    def __init__(self, parent_node, state, action, sampledAction, hidden):
         self.parent = parent_node
         self.children = []
         self.state = state
         self.action = action
         self.hidden = hidden
+        self.sampledAction = sampledAction
         
     def addChild(self, child):
         self.children.append(child)
         
 class Tree(object):
     
-    def __init__(self, initialState, forwardModel, simPolicy, valueF, env,maxDepth=5, branchingFactor=3):
+    def __init__(self, initialState, forwardModel, simPolicy, valueF, env, maxDepth=5, branchingFactor=3):
         self.simPolicy = simPolicy
         self.maxDepth, self.branchFactor = maxDepth, branchingFactor
         self.forwardModel = forwardModel
         self.valueF = valueF
         self.allStates = [initialState]
         self.allActions = []
+        self.allNodes = []
         self.env = env
-#         print('Generating growth')
         # Generate Tree
         self.forwardModel.reInitialize(1)
-        parent = Node(None,initialState,None, self.forwardModel.hidden)
+        parent = Node(None, initialState, None, None, self.forwardModel.hidden)
+        self.allNodes.append(parent)
         self.tree_head = self.grow(parent,0,self.branchFactor)
-        #self.tAllStates = tf.stack(self.allStates)
         # Get leaves
-#         print('Getting leaves')
         q, self.leaves = [ parent ], []
         while len(q) >= 1:
             currNode = q.pop()
             for child in currNode.children:
                 if len( child.children ) == 0: self.leaves.append( child )
                 else: q.append( child )
-        #print(self.leaves)
     
     def getPathFromLeaf(self,leafNumber):
         leaf = self.leaves[leafNumber]
@@ -180,71 +185,108 @@ class Tree(object):
         actions = [leaf.action]
         currNode = leaf
         while not currNode.parent is None:
-            #print(currNode.state)
+            path.append(currNode.parent.state)
+            if not currNode.parent.action is None:
+                actions.append(currNode.parent.action)
+            currNode = currNode.parent
+        return (list(reversed(path)), list(reversed(actions)))
+
+
+    def getPathFromNode(self,nodeNumber):
+        node = self.allNodes[nodeNumber]
+        path = [node.state]
+        actions = [node.action]
+        currNode = node
+        while not currNode.parent is None:
             path.append(currNode.parent.state)
             if not currNode.parent.action is None:
                 actions.append(currNode.parent.action)
             currNode = currNode.parent
         return (list(reversed(path)),list(reversed(actions)))
+
     
     def grow(self,node,d,b,verbose=False):
         if verbose: print('Grow depth: ',d)
         if verbose: self.env.printState(node.state[0].data.numpy())
         if d == self.maxDepth : return node
-        #print(d)
         for i in range(b):
             # Sample the current action
             hard_action, soft_a_s = self.simPolicy.sample(node.state)
             a_s =  [torch.squeeze(hard_action)]
-            #print(a_s)
-            #concat_vec = torch.cat([node.state, a_s], 1)
-            #print("concat_vec",concat_vec.data.numpy(),d)
             inital_state =  torch.squeeze(node.state)
-            #print(inital_state.shape)
-            #print(a_s[0].shape)
-            #self.forwardModel.reInitialize(1)
             self.forwardModel.setHiddenState(node.hidden)
             current_state, _, current_hidden = self.forwardModel.forward(inital_state,a_s, 1)
             # Build the next subtre
             current_state = current_state.unsqueeze(dim=0)
-            
             self.allStates.append(current_state)
             self.allActions.append(a_s)
-            if verbose: print("int_state at depth",d)
-            if verbose: self.env.printState(node.state[0].data.numpy())
-            if verbose: print("a_s at depth ",d," and breath",i)
-            #if verbose: self.env.printAction(a_s[0])
-            #self.env.printAction(a_s[0])
-            if verbose: print("curr_state at depth",d)
-            if verbose: self.env.printState(current_state[0].data.numpy())
-            node.addChild( self.grow( Node(node, current_state, [soft_a_s], current_hidden), d+1, b) )
+            if verbose:
+                print("int_state at depth",d)
+                self.env.printState(node.state[0].data.numpy())
+                print("a_s at depth ",d," and breath",i)
+                self.env.printAction(a_s[0])
+                self.env.printAction(a_s[0])
+                print("curr_state at depth",d)
+                self.env.printState(current_state[0].data.numpy())
+            childNode = Node(node, current_state, [soft_a_s], [hard_action], current_hidden)
+            self.allNodes.append( childNode )
+            node.addChild( self.grow( childNode, d+1, b) )
         return node
     
-    def getBestPlan(self):
+    #
+    def getBestPlanFromLeaves(self):
         bestInd, bestVal = 0, avar(torch.FloatTensor( [float('-inf')])) #float('-inf')
         for i, leaf in enumerate(self.leaves):
             currVal = self.valueF(leaf.state)
-            #print('State')
-            #self.forwardModel.printState(leaf.state[0])
-            #print('Value',currVal)
             if currVal.data.numpy() > bestVal.data.numpy():
                 bestInd = i
                 bestVal = currVal
-        #print(bestVal)
         return self.getPathFromLeaf( bestInd )
-    
-    def getLossFromLeaves(self, alpha=0.5, lambda_h=-0.025, useHolder=False, holderp=-5.0):
-        if not useHolder: holderp = 1.0
-        nleaves = len(self.leaves)
-        totalInverseValue = avar(torch.FloatTensor([0.0]))
-        totalEntropy = avar(torch.FloatTensor([0.0]))
-        for i, leaf in enumerate(self.leaves):
-            totalInverseValue += -self.valueF( leaf.state ).pow( holderp )  
-            totalEntropy += -torch.sum(leaf.action[0] * torch.log(leaf.action[0]))
-            #print(leaf.action[0].data.numpy().argmax(),-self.valueF( leaf.state ).data[0])
-        totalLoss = alpha*(totalInverseValue / nleaves).pow( 1.0 / holderp) + lambda_h * totalEntropy
-        return totalLoss  #torch.min(totalLosses)
+
+    def getBestPlan(self):
+        bestInd, bestVal = 0, avar(torch.FloatTensor( [float('inf')])) #float('-inf')\n",
+        for i, node in enumerate(self.allNodes):
+            currVal = node.loss #self.valueF(leaf.state)\n",
+            # print('cv',i,':',currVal)
+            #print('State')\n",
+            #self.forwardModel.printState(leaf.state[0])\n",
+            #print('Value',currVal)\n",
+            if currVal.data.numpy() < bestVal.data.numpy():
+                bestInd = i
+                bestVal = currVal
+        # print("index",bestInd)
+        return self.getPathFromNode( bestInd )
         
+    def getLossFromAllNodes(self, alpha=0.5, lambda_h=-0.025, useHolder=False, holderp=-5.0, useOnlyLeaves=False):
+        targetNodes = self.allNodes
+        if useOnlyLeaves: targetNodes = self.leaves
+        totalInverseValue = avar(torch.FloatTensor([0.0]))
+        totalEntropy      = avar(torch.FloatTensor([0.0]))
+        if not useHolder: holderp = 1.0
+        nNodes = len(targetNodes)
+        # print('N_NODES',nNodes)
+        for i, node in enumerate(targetNodes):
+            if i == 0: 
+                node.loss = avar(torch.FloatTensor( [float('inf')]))
+                continue
+            node.loss = -self.valueF( node.state )
+            totalInverseValue += node.loss.pow( holderp )
+            if not node.action is None:
+                totalEntropy += -torch.sum(node.action[0] * torch.log(node.action[0]))
+        # print('TIV',totalInverseValue)
+        totalLosses = alpha * (totalInverseValue / nNodes).pow(1.0 / holderp) + lambda_h * totalEntropy
+        return totalLosses 
+
+    # Call before getting best plan
+    def measureLossAtTestTime(self, useOnlyLeaves=False):
+        targetNodes = self.allNodes
+        if useOnlyLeaves: targetNodes = self.leaves
+        for i, node in enumerate(targetNodes):
+            if i == 0: 
+                node.loss = avar(torch.FloatTensor( [float('inf')]))
+                continue
+            node.loss = -self.valueF( node.state )
+
 
 def main():
 
@@ -267,20 +309,36 @@ def main():
     SimPolicy = SimulationPolicy(exampleEnv)
     # Run training
     if runTraining:
-        SimPolicy.trainSad(exampleEnv, ForwardModel,printActions=True,maxDepth=3,treeBreadth=2) 
+        SimPolicy.trainSad(
+            exampleEnv, 
+            ForwardModel, 
+            printActions=True, 
+            maxDepth=3, 
+            treeBreadth=2, 
+            eta_lr=0.0005,
+            trainIters=500,
+            alpha=0.5,
+            lambda_h=-0.025,
+            useHolder=False,
+            holderp=-6.0, 
+            useOnlyLeaves=False
+        )
+         
         # 
         s_0 = torch.unsqueeze(avar(torch.FloatTensor(exampleEnv.getStateRep())), dim=0)
-        tree = Tree(s_0, ForwardModel, SimPolicy, greedy_cont_valueF, exampleEnv, 2, 3)
+        tree = Tree(s_0, ForwardModel, SimPolicy, greedy_valueF, exampleEnv, maxDepth=3, branchingFactor=2)
+        tree.measureLossAtTestTime()
         states, actions = tree.getBestPlan()
         print('Final Actions')
         for i in range(len(actions)):
-            print(actions[i][0].data.numpy().argmax())
+            jq = actions[i][0].data.numpy().argmax()
+            print('A'+str(i)+':',jq,NavigationTask.actions[jq])
 
 
-
+############################
 if __name__ == "__main__":
     main()
-
+############################
 
 
 
